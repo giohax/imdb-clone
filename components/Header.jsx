c68891106510337706797f322f2881a8445fac7b
giohax <giovannifgonzaga@gmail.com>
2022-11-04 10:32:37 -0500

    style the header section

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,18 +10,19 @@ import {
 
 const Header = () => {
     return (
-        <div>
-            <div>
-                <HeaderIcon Icon={HomeIcon} title="HOME" />
-                <HeaderIcon Icon={UserIcon} title="CONTACT" />
-                <HeaderIcon Icon={PhoneIcon} title="ACCOUNT" />
-                <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+        <div className="bg-gray-700 text-gray-200 flex flex-col sm:flex-row items-center sm:justify-between p-3 select-none">
+            <div className="flex mb-5 sm:mb-0">
+                <HeaderIcon Icon={HomeIcon} title="Home" />
+                <HeaderIcon Icon={UserIcon} title="Contact" />
+                <HeaderIcon Icon={PhoneIcon} title="Account" />
+                <HeaderIcon Icon={InformationCircleIcon} title="About" />
             </div>
 
             <Image
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/440px-IMDB_Logo_2016.svg.png"
                 width={100}
                 height={100}
+                className="cursor-pointer active:brightness-110"
             />
         </div>
     );
--- components/HeaderIcon.jsx
+++ components/HeaderIcon.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const HeaderIcon = ({ Icon, title }) => {
     return (
-        <div>
-            <Icon className="h-8 w-8" />
-            <p>{title}</p>
+        <div className="mx-4 flex flex-col hover:text-white active:text-red-400 cursor-pointer lg:mx-6">
+            <Icon className="h-5" />
+            <p className="my-0 text-sm">{title}</p>
         </div>
     );
 };
