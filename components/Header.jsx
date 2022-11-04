5cf9167756ea147c1ad4e2cf397f28812f5d24df
giohax <giovannifgonzaga@gmail.com>
2022-11-04 10:14:11 -0500

    create the header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
+import {
+    HomeIcon,
+    UserIcon,
+    PhoneIcon,
+    InformationCircleIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div>
+            <div>
+                <HeaderIcon Icon={HomeIcon} title="HOME" />
+                <HeaderIcon Icon={UserIcon} title="CONTACT" />
+                <HeaderIcon Icon={PhoneIcon} title="ACCOUNT" />
+                <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+            </div>
+
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/440px-IMDB_Logo_2016.svg.png"
+                width={100}
+                height={100}
+            />
+        </div>
+    );
 };
 
 export default Header;
