3eebb440cc9338ab97850ceb8915b035e642d0b2
giohax <giovannifgonzaga@gmail.com>
2022-11-05 04:45:05 -0500

    create card component

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 const Results = ({ results }) => {
     return (
         <div>
-            {results.map((results) => (
-                <h1>{results.title}</h1>
+            {results.map((result) => (
+                <Card key={result.id} result={result} />
             ))}
         </div>
     );
